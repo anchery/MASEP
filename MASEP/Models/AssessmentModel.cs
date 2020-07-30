bdfc974ebfec1ed3f615157e1506944d35554e08
Finney Thomas <finney.thomas@hotmail.com>
2020-07-30 17:38:20 -0500

    gender

---
+++ MASEP/Models/AssessmentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MASEP.Models
+{
+    public class AssessmentModel
+    {
+        public int Gender { get; set; }
+        public int Race { get; set; }
+        public int MaritalStat { get; set; }
+        public int Income { get; set; }
+        public DateTime DOB { get; set; }
+        public int Education { get; set; }
+        public int EmpStat { get; set; }
+    }
+}
