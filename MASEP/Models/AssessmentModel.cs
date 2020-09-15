978ed78237c7442574003549e17af8a7e0112533
Finney Thomas <finney.thomas@hotmail.com>
2020-09-15 16:59:23 -0500

    Added Q24-27

--- MASEP/Models/AssessmentModel.cs
+++ MASEP/Models/AssessmentModel.cs
@@ -11,17 +11,29 @@ namespace MASEP.Models
     {
         [Required(ErrorMessage ="Gender is required.")]
         public string Gender { get; set; } = null;
+        
+        [Required(ErrorMessage = "Race is required.")]
         public string Race { get; set; } = null;
+        
+        [Required(ErrorMessage = "Marital Status is required.")]
         public string MaritalStat { get; set; } = null;
+        
+        [Required(ErrorMessage = "Household Income is required.")]
         public string Income { get; set; } = null;
 
         [Required(ErrorMessage = "Date of birth is required.")]
         public string DOB { get; set; } = "MM/dd/yyyy";
 
+        [Required(ErrorMessage = "Age is required.")]
         public string Age { get; set; } = null;
 
+        [Required(ErrorMessage = "Education is required.")]
         public string Education { get; set; } = null;
+        
+        [Required(ErrorMessage = "Employment Status is required.")]
         public string EmpStat { get; set; } = null;
+
+
         public string Q74a { get; set; } = null;
         public string Q74b { get; set; } = null;
         public string Q74c { get; set; } = null;
